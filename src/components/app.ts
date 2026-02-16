eabbbf9ddedb6fce735405ca857b0c29e4c2c794
albertnis <4309984+albertnis@users.noreply.github.com>
2026-02-17 08:38:21 +1300

    Use header button for connection info and control

--- src/components/app.ts
+++ src/components/app.ts
@@ -4,18 +4,35 @@ import { filter } from "rxjs";
 
 const elements = {
 	app: queryElement("#app"),
+	connectionChip: queryElement(".connectionChip"),
 };
 
 client.events
 	.pipe(
 		filter((event) => event.type === "CONNECT" || event.type === "DISCONNECT"),
 	)
-	.subscribe((event) => renderAppConnectionStatus(event.type === "CONNECT"));
+	.subscribe((event) => {
+		if (event.type === "CONNECT") {
+			renderConnected(event.connectionType);
+		} else {
+			renderDisconnected();
+		}
+	});
 
-function renderAppConnectionStatus(connected: boolean) {
-	if (connected) {
-		elements.app.setAttribute("data-connected", "");
+function renderConnected(method: "bluetooth" | "serial") {
+	elements.app.classList.add("connected");
+	elements.connectionChip.removeAttribute("disabled");
+
+	if (method === "bluetooth") {
+		elements.app.classList.add("connected-bluetooth");
 	} else {
-		elements.app.removeAttribute("data-connected");
+		elements.app.classList.add("connected-serial");
 	}
+}
+
+function renderDisconnected() {
+	elements.app.classList.remove("connected");
+	elements.app.classList.remove("connected-bluetooth");
+	elements.app.classList.remove("connected-serial");
+	elements.connectionChip.setAttribute("disabled", "");
 }
--- src/components/connectionInformation.ts
+++ src/components/connectionInformation.ts
@@ -1,4 +1,4 @@
-import { stateUpdates } from "@/client";
+import { client, stateUpdates } from "@/client";
 import { queryElement } from "@/dom/queryTemplateElement";
 import { filter, map } from "rxjs";
 
@@ -6,7 +6,9 @@ const elements = {
 	firmwareVersion: queryElement(".connectionInformation-firmwareVersion"),
 	macAddress: queryElement(".connectionInformation-macAddress"),
 	protocolVersion: queryElement(".connectionInformation-protocolVersion"),
-	disconnectButton: queryElement(".connectionInformation-disconnectButton"),
+	disconnectButton: queryElement(".disconnectButton"),
+	restartButton: queryElement(".restartButton"),
+	connectedPopover: queryElement("#connectedPopover"),
 };
 
 stateUpdates
@@ -36,6 +38,16 @@ stateUpdates
 		elements.protocolVersion.innerHTML = x.toString();
 	});
 
+elements.restartButton.addEventListener("click", () => {
+	client.sendCommand({
+		type: "ENABLE_CONFIGURATION_COMMAND",
+	});
+
+	client.sendCommand({
+		type: "RESTART",
+	});
+});
+
 elements.disconnectButton.addEventListener("click", () => {
-	window.location.reload();
+	client.disconnect().catch(console.error);
 });
